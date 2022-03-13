bde380a54d5b932e65fabff0bf3f508a8e42fdcd
mokhs00 <mokhs00@naver.com>
2022-03-13 22:27:29 +0900

    add : 컴포넌트  생성 완료
    - TodoTemplate
    - TodoHead
    - TodoList
    - TodoItem
    - TodoCreate

---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import TodoItem from "./TodoItem";
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성" done={true}></TodoItem>
+      <TodoItem text="프로젝트 생성" done={false}></TodoItem>
+    </TodoListBlock>
+  );
+}
+
+export default TodoList;
